d0fceeb82c291058f067dd0d4dc947dc35dab72a
AlexPerritos <90848592+AlexPerritos@users.noreply.github.com>
2021-09-30 16:53:47 +0200

    Jiji

--- canonespolares.py
+++ canonespolares.py
@@ -1,1 +1,2 @@
 print("Se diseñan cañones de cangrejos en escandinavia como protección contra la Comisión Republicana Norteña de los osos polares.")
+print("NO SON OSOS POLARES SON OSOS PARDOS PINTADOS DE BLANCO")
